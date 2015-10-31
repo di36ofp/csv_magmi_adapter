75a5c191352f74c218512d3322a45f5149fb35b6
Diego <info@diego-suarez.es>
2015-10-31 15:50:01 +0100

    set up config requirements

---
+++ adapter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * CSV MAGMI adapter
+ *
+ * @author Diego P. Suárez <info@diego-suarez.es>
+ * @package CSV_Magmi_adapter
+ * @copyright Copyright (c) 2015 Tiempo Libre.
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Adapter{
+
+  public $csv_import_file;
+
+  private $csv_export_file;
+
+}
+
+$adapter = new Adapter();
